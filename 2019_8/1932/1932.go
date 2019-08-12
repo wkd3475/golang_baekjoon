3fe71cc00e6cf47aa557db9a71f2dd520f39567c
wkd3475 <wkd3475@gmail.com>
2019-08-12 20:42:49 +0900

    update

--- 2019_8/1932/1932.go
+++ 2019_8/1932/1932.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
+var in = bufio.NewScanner(os.Stdin)
+
+func nextInt() int {
+	in.Scan()
+	r := 0
+	for _, c := range in.Bytes() {
+		r *= 10
+		r += int(c - '0')
+	}
+	return r
+}
+
 func main() {
+	in.Split(bufio.ScanWords)
 	var N, max int
-	fmt.Scanln(&N)
+	N = nextInt()
 	var memory [550][550]int
 
 	memory[0][0] = 0
@@ -14,7 +29,7 @@ func main() {
 
 	for i := 1; i <= N; i++ {
 		for j := 1; j <= i; j++ {
-			fmt.Scan(&memory[i][j])
+			memory[i][j] = nextInt()
 			if j == 1 {
 				memory[i][j] = memory[i-1][j] + memory[i][j]
 			} else if j == i {
